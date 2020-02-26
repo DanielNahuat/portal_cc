5ed84c7bde537b9170e48e24afb8084bf930e1e5
Sheila <sheilait@yascemail.com>
2020-02-26 13:52:36 -0600

    Migrations Candidates Vacancy

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+//Display Index Page Vacancies
+Route::get('/vacancies', 'VacancyController@index');
