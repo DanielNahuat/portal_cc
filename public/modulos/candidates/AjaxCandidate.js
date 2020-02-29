93494b258d12ba6d40c166c2c06bf72eb8f17d78
Sheila <sheilait@yascemail.com>
2020-02-29 14:00:01 -0600

    Candidate Desing form

--- public/modulos/candidates/AjaxCandidate.js
+++ public/modulos/candidates/AjaxCandidate.js
@@ -7,6 +7,29 @@ $(document).ready(function(){
     $('.nameDeli').html(nameDeli);
     $('#sidebar10').addClass('active');  
 
+      //get base URL *********************
+      var url = $('#url').val();
+
+
+      //display modal form for creating new product *********************
+      $('#btn_add').click(function(){
+          $('#btn-save').val("add");
+          $('#candidateForm').trigger("reset");
+          $('#myModal').modal('show');
+      });
+
+       //display modal form for product EDIT ***************************
+    $(document).on('click','.open_modal',function(){
+        $('#candidateForm').trigger("reset");
+        var candidate_id = $(this).val();
+        var my_url = url + '/' + candidate_id;
+
+            actions.show(my_url);
+       
+    });
+
+  
+
 
 
 
